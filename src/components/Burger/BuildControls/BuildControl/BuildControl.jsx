adf92c133fb981da368557d06177a05b61e26dd1
TareqJudehGithub <tareq.joudeh@gmail.com>
2020-05-03 02:36:53 +0300

    Added Build Controls, where we can now add and remove ingredients to the Burger Builder dynamically.

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./BuildControl.module.css";
+
+const BuildControl = (props) => {
+     return (
+          <div className={classes.BuildControl}>
+               <div className={classes.Label}>{props.label}</div>
+               <button 
+               className={classes.Less}
+               onClick={props.remove}
+               disabled={props.removerDisabled} //disabled is a default html
+               >
+                    &#8826;
+               </button>
+
+               <button 
+               className={classes.More}
+               onClick={props.add}>    
+                    &#8827;
+               </button>
+          </div>
+     )
+}
+export default BuildControl;
--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,11 +4,26 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 import classes from "./Burger.module.css";
 
 const Burger = (props) => {
+     let transformedIngredients = Object.keys(props.ingredients)
+          .map(igKey => {  
+               return [...Array(props.ingredients[igKey])].map((_, index) => {
+                    return <BurgerIngredient 
+                    key={igKey + index} 
+                    type={igKey} 
+                    />
+               })               
+          })
+          .reduce((arr, el) => {  //to flatten the array;
+               return arr.concat(el);
+          });
+          if(transformedIngredients.length === 0) {
+               transformedIngredients = <p>Please add ingredients..</p>
+          }
+     
      return (
           <div className={classes.Burger}>
                <BurgerIngredient type="bread-top"/>
-               <BurgerIngredient type="cheese"/>
-               <BurgerIngredient type="meat"/>
+               {transformedIngredients}
                <BurgerIngredient type="bread-bottom"/>
           </div>
      )
